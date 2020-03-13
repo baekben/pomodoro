b79954799fed87a382f9c0afdc3423bf1e690bda
Ben <ben10baek@hotmail.com>
2020-03-12 20:54:21 -0700

    update

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -9,6 +9,10 @@ var pause = false;
 var timeInterval;
 var timeLeft;
 var menu;
+<<<<<<< HEAD
+=======
+var rest = 5;
+>>>>>>> Recovering from the git fiasco
 
 function remainingTime(end) {
   var time = Date.parse(end) - Date.parse(new Date());
@@ -22,6 +26,12 @@ function menuLength(length, id) {
   // 1-5 for breaks
   if (id == 'dropdown1') {
     countLength = length;
+<<<<<<< HEAD
+=======
+    sessionTime = length; //holds the choosen session length
+  } else {
+    rest = length;
+>>>>>>> Recovering from the git fiasco
   }
   menu = document.getElementById(`${id}`);
   for (let i = 0; i < length; i++) {
@@ -50,6 +60,13 @@ window.onclick = function(event) {
             this.countLength < 10 ? '0' + this.countLength : this.countLength;
           timer.innerHTML = `${this.countLength}:00`;
         }
+<<<<<<< HEAD
+=======
+        // if (event.target.matches('#break')) {
+        //   var select = document.getElementById('dropdown2');
+        //   this.rest = select[select.selectedIndex].value;
+        // }
+>>>>>>> Recovering from the git fiasco
         openDropdown.classList.remove('show');
         menu.innerHTML = ''; //deletes menu
       }
@@ -73,22 +90,28 @@ function startTimer() {
 }
 
 function stopTimer() {
-  if (!pause) {
+  if (!pause && timeLeft > 0) {
     resume.innerHTML = 'Resume';
     pause = true;
-    clearInterval(timeInterval);
-    timeLeft = remainingTime(endTime).totalTime;
-    console.log('Timer stopped ...');
   }
+  clearInterval(timeInterval);
+  timeLeft = remainingTime(endTime).totalTime;
+  console.log('Timer stopped ...');
 }
 function runTimer() {
   var time = remainingTime(endTime);
   let minutes = time.minutes;
   let seconds = time.seconds;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
   seconds = seconds < 10 ? '0' + seconds : seconds;
   timer.innerHTML = `${minutes}:${seconds}`;
   if (time.totalTime <= 0) {
     clearInterval(timeInterval);
+<<<<<<< HEAD
+=======
+    stopTimer();
+    breakTime();
+>>>>>>> Recovering from the git fiasco
     //run break
   }
 }
@@ -106,4 +129,38 @@ function reset() {
   timer.innerHTML = `${countLength}:00`;
   resume.innerHTML = 'Start';
   console.log('Timer reset ...');
+}
+
+function breakTime() {
+  var status = document.getElementById('status');
+  if (status.innerHTML == 'Working') {
+    breakNum = rest; //amount of breaks
+    breakNum -= 1;
+    pause = false;
+    timeLeft = 0;
+    console.log(breakNum);
+    clearInterval(timeInterval);
+    if (breakNum > 1) {
+      alert('Time for a quick break');
+      countLength = 5;
+    } else if (breakNum == 1) {
+      alert(
+        "You have completed four sessions so far! \n You've earned a 30 minute break!"
+      );
+      countLength = 30;
+    }
+    if (breakNum == 0) {
+      //default rest amount
+      breakNum = 5;
+    }
+    status.innerHTML = 'Break Time';
+  } else {
+    status.innerHTML = 'Working';
+    countLength = sessionTime;
+  }
+  timer.innerHTML = `${countLength}:00`;
+
+  //have a counter for tracking breaks
+  //5 minute breaks
+  //if 4 breaks have occured then 5th break == 30 min
 }
