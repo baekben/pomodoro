14a3aaf18c9024ac2a958f08ed61e02bbcb1ea38
Ben <ben10baek@hotmail.com>
2020-03-12 20:17:05 -0700

    Recovering from the git fiasco

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -1,13 +1,15 @@
 const timer = document.getElementById('timer');
 const resume = document.getElementById('start');
-// starting time length will be 25 minutes
-var countLength = 25;
+// starting time length will be default at 25
+var countLength = 25; //time can be changed
 var startTime;
 var endTime;
 
 var pause = false;
 var timeInterval;
 var timeLeft;
+var menu;
+var rest = 5;
 
 function remainingTime(end) {
   var time = Date.parse(end) - Date.parse(new Date());
@@ -19,28 +21,45 @@ function remainingTime(end) {
 function menuLength(length, id) {
   //create list of number from 1-25 for sessions
   // 1-5 for breaks
-  var menu = document.getElementById(`${id}`);
+  if (id == 'dropdown1') {
+    countLength = length;
+    sessionTime = length; //holds the choosen session length
+  } else {
+    rest = length;
+  }
+  menu = document.getElementById(`${id}`);
   for (let i = 0; i < length; i++) {
     var item = document.createElement('option');
     item.setAttribute('value', `${i + 1}`);
+    item.setAttribute('id', 'number');
     var option = document.createTextNode(`${i + 1}`);
     item.appendChild(option);
     menu.appendChild(item);
   }
   document.getElementById(`${id}`).classList.toggle('show');
-
-  //hide the list
-  //show list if clicked
 }
 
+//closes out of options if clicked anywhere besides options
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName('drop-content');
     for (let i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
+        if (event.target.matches('#number')) {
+          //choosing new minutes
+          var select = document.getElementById('dropdown1');
+          this.countLength = select[select.selectedIndex].value;
+          this.countLength =
+            this.countLength < 10 ? '0' + this.countLength : this.countLength;
+          timer.innerHTML = `${this.countLength}:00`;
+        }
+        // if (event.target.matches('#break')) {
+        //   var select = document.getElementById('dropdown2');
+        //   this.rest = select[select.selectedIndex].value;
+        // }
         openDropdown.classList.remove('show');
-        this.menu.innerHTML = '';
+        menu.innerHTML = ''; //deletes menu
       }
     }
   }
@@ -62,22 +81,26 @@ function startTimer() {
 }
 
 function stopTimer() {
-  if (!pause) {
+  if (!pause && timeLeft > 0) {
     resume.innerHTML = 'Resume';
     pause = true;
-    clearInterval(timeInterval);
-    timeLeft = remainingTime(endTime).totalTime;
-    console.log('Timer stopped ...');
   }
+  clearInterval(timeInterval);
+  timeLeft = remainingTime(endTime).totalTime;
+  console.log('Timer stopped ...');
 }
 function runTimer() {
   var time = remainingTime(endTime);
   let minutes = time.minutes;
   let seconds = time.seconds;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
   seconds = seconds < 10 ? '0' + seconds : seconds;
   timer.innerHTML = `${minutes}:${seconds}`;
   if (time.totalTime <= 0) {
     clearInterval(timeInterval);
+    stopTimer();
+    breakTime();
+    //run break
   }
 }
 
@@ -88,4 +111,38 @@ function reset() {
   timer.innerHTML = `${countLength}:00`;
   resume.innerHTML = 'Start';
   console.log('Timer reset ...');
+}
+
+function breakTime() {
+  var status = document.getElementById('status');
+  if (status.innerHTML == 'Working') {
+    breakNum = rest; //amount of breaks
+    breakNum -= 1;
+    pause = false;
+    timeLeft = 0;
+    console.log(breakNum);
+    clearInterval(timeInterval);
+    if (breakNum > 1) {
+      alert('Time for a quick break');
+      countLength = 5;
+    } else if (breakNum == 1) {
+      alert(
+        "You have completed four sessions so far! \n You've earned a 30 minute break!"
+      );
+      countLength = 30;
+    }
+    if (breakNum == 0) {
+      //default rest amount
+      breakNum = 5;
+    }
+    status.innerHTML = 'Break Time';
+  } else {
+    status.innerHTML = 'Working';
+    countLength = sessionTime;
+  }
+  timer.innerHTML = `${countLength}:00`;
+
+  //have a counter for tracking breaks
+  //5 minute breaks
+  //if 4 breaks have occured then 5th break == 30 min
 }
